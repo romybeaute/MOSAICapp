fc2476f6d9b6dd00aa335dc467a952612155aca8
romybeaute <romybeaute.univ@gmail.com>
2026-05-21 12:15:06 +0200

    changed for 4b full set, reduce params range after first search

--- run_optuna.sh
+++ run_optuna.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=mosaic_optuna
-#SBATCH --partition=short
+#SBATCH --partition=general
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 #SBATCH --output=optuna_%j.log
 
 module load CUDA/12.1.1
@@ -18,9 +18,8 @@ python run_optuna.py \
     --text-col cleaned_report \
     --sentences \
     --n_trials 100 \
-    --min-cluster-size 20 100 \
-    --min-samples 10 60 \
-    --n-neighbors 5 50 \
+    --min-cluster-size 20 70 \
+    --min-samples 10 50 \
+    --n-neighbors 15 35 \
     --target-min 40 \
-    --target-max 110 \
-    --subsample 15000
+    --target-max 110
